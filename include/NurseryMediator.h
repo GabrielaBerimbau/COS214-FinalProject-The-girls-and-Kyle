fa3ae1d6129d0f9b87a82eb5dbc64e294827f3a3
kahlanhgrmn <kahlanhagerman@gmail.com>
2025-10-28 21:56:26 +0200

    fixed the fact that the plant is removed from sf and they can put it back

--- include/NurseryMediator.h
+++ include/NurseryMediator.h
@@ -6,6 +6,7 @@
 
 class Colleague;
 class Plant;
+class Customer;
 
 /**
  * @class NurseryMediator
@@ -74,6 +75,21 @@ class NurseryMediator{
          * @param colleague The colleague to remove
          */
         void removeColleague(Colleague* colleague);
+
+        /**
+         * @brief Remove a plant from its current location and transfer it to the customer
+         * @param plantName Name of the plant to transfer
+         * @param customer The customer receiving the plant
+         * @return true if successful, false if not
+         */
+        bool transferPlantToCustomer(std::string plantName, Customer* customer);
+        
+        /**
+         * @brief Return a plant from customers cart back to sales floor
+         * @param plant The plant to return
+         * @return true if successful, false if not
+         */
+        bool returnPlantToDisplay(Plant* plant);
 };
 
 #endif
